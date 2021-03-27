c0a84f2fec6497da7452ee49bfc8f3d00415b8f9
Mambetov-Mn <mambetov.mn@gmail.com>
2021-03-27 17:35:39 +0600

    modified of nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'Beta 0.1 %s - Научная библиотека имени. Касыма Тыныстанова',
+    titleTemplate: '[Beta 0.1] %s - Научная библиотека имени. Касыма Тыныстанова',
     title: 'Главная',
     meta: [
       { charset: 'utf-8' },
