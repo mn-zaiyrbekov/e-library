a58138a5ebb722054dd5a9ada116be6e33ba30a1
mn-zaiyrbekov <mn.zaiyrbekov@gmail.com>
2021-04-07 19:40:27 +0600

    add category to books in leftBar

--- client/components/Globals/LeftBar.vue
+++ client/components/Globals/LeftBar.vue
@@ -20,18 +20,33 @@
           <v-list-item-title>Главная</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-      <v-list-item
-        router
-        exact
-        to="/books"
-      >
-        <v-list-item-action>
-          <v-icon>mdi-bookshelf</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>Книги</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      
+        <v-list-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>
+                <v-list-item-icon>
+                  <v-icon>mdi-bookshelf</v-icon>
+                </v-list-item-icon>
+                Книги
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="(genre, key) in genre"
+            :key="key"
+            link
+            :to="`/books/${genre._id}`"
+            exact
+          >
+            <v-list-item-subtitle class="white--text">
+              <v-icon class="mr-2">mdi-book-check-outline</v-icon>
+              {{genre.name}}
+            </v-list-item-subtitle>
+          </v-list-item>
+        </v-list-group>
+    
       <v-list-item
         router
         exact
@@ -49,6 +64,7 @@
   </v-navigation-drawer>
 </template>
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'LeftBar',
   props: {
@@ -64,6 +80,19 @@ export default {
     drawer() {
       this.drawerState = !this.drawerState
     }
+  },
+  computed: {
+    ...mapGetters({
+      genre: 'books/getBookGenre'
+    })
+  },
+  methods: {
+    ...mapActions({
+      fetchGenre: 'books/getAllGenreBooks'
+    })
+  },
+  created() {
+    this.fetchGenre()
   }
 }
 </script>
