2255aa392d70eda310d0a475e28f3a448dcc2a22
mn-zaiyrbekov <mn.zaiyrbekov@gmail.com>
2021-04-08 20:26:45 +0600

    create genre component for admin

---
+++ client/pages/admin/genre/add.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'add'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/pages/admin/genre/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-row>
+      <v-col class="col-md-6 col-12">
+        <v-card-title class="orange accent-4 white--text">
+          Жанры книг
+        </v-card-title>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-12">
+      <GenreCard />
+    </v-row>
+  </div>
+</template>
+
+<script>
+import GenreCard from '@/components/admin/GenreCard'
+export default {
+  name: 'genre',
+  layout: 'adminlayout',
+  components: { GenreCard },
+  head() {
+    return {
+      title: 'Жанры книг'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
