5278638184dd09ab5a9c0cb01ae8ed98271d487f
Mirlan <mn.zaiyrbekov@gmail.com>
2021-05-10 11:28:05 +0600

    completed searcging books

--- client/components/Globals/Search.vue
+++ client/components/Globals/Search.vue
@@ -4,7 +4,6 @@
       v-model="select"
       :loading="searchLoading"
       :search-input.sync="search"
-      :items="items"
       hide-no-data
       hide-details
       label="Введите название книги"
@@ -12,7 +11,15 @@
       solo-inverted
       cache-items
     >
+          <!-- :items="items" -->
     </v-autocomplete>
+    <v-expand-transition>
+     <div class="search-link" v-if="search">
+       <div v-for="(book, key) in items" :key="key">
+         <nuxt-link :to="`/books/${book._id}`">{{book.booksTitle}}</nuxt-link>
+       </div>
+     </div>
+    </v-expand-transition>
   </div>
 </template>
 
@@ -36,16 +43,30 @@
         this.searchLoading = true
         const res = await this.$axios.post('/books/book/search', { search: val } )
         if (res.data.success) {
-          res.data.subject.filter(e => {
-            this.items = [e.booksTitle]
-          })
+          this.items = res.data.subject
+          // res.data.subject.filter(e => {
+          //   this.items = [e.booksTitle]
+          // })
         }
         this.searchLoading = false
       }
+    },
+    mounted() {
+      console.log(this.items)
     }
   }
 </script>
 
 <style scoped>
-
+.search-link{
+  position: absolute;
+  width: 100%;
+  min-height: 100%;
+  background: #fff;
+  padding: 10px;
+}
+.search-link a {
+  margin-bottom: 10px;
+  color: #000;
+}
 </style>
